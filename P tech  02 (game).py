0efa0caf9e46363fc41a4a1fb86f2306e83623d6
saladetomateoignonchef <150328496+saladetomateoignonchef@users.noreply.github.com>
2023-11-09 10:50:27 +0100

    Update P tech  02 (game).py

--- P tech  02 (game).py
+++ P tech  02 (game).py
@@ -11,5 +11,5 @@ while True:
     if valeur_saisie>valeur_rechercher:
         print ("le chiffre est plus petit")
 
-
+#Bonjour je suis un élevé de 1er pro et j'ai créé ce jeu très basic, mais c'est la première fois que je touche à du code. J'ai fait ce code en classe sur thonny avec l'entreprise IBM. Merci
                     
